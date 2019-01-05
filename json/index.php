207f408f1df18c2de25ed7b9a40ecef479c0a1e6
fauziarfian <44840780+fauziarfian@users.noreply.github.com>
2019-01-05 20:02:46 +0700

    Delete index.php

--- json/index.php
+++
@@ -1,8 +0,0 @@
-{
-"Nama":"Fauzi Arfian",
-"Alamat":"Jl.Bojong Pondok Terong Rt004/013 Kec.Cipayung Kota Depok",
-"Hobi":"Sepak Bola",
-"Status":"Belum Menikah",
-"Sekolah":"SMK Assalamah",
-"Keahlian":"Memasak"
-}
