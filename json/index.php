f5c2c847ad0f8554848692da98f7e3130ec92097
fauziarfian <44840780+fauziarfian@users.noreply.github.com>
2019-01-05 20:01:50 +0700

    Create index.php

---
+++ json/index.php
@@ -0,0 +1,8 @@
+{
+"Nama":"Fauzi Arfian",
+"Alamat":"Jl.Bojong Pondok Terong Rt004/013 Kec.Cipayung Kota Depok",
+"Hobi":"Sepak Bola",
+"Status":"Belum Menikah",
+"Sekolah":"SMK Assalamah",
+"Keahlian":"Memasak"
+}
